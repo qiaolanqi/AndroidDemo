962ede93073b2ff3739e36b754b774675d77b0c3
lyj <061304011116lyj@163.com>
2016-09-23 17:39:31 +0800

    add

--- AndroidDemo/src/com/yuejiaoliapidemo/app/DrawableActivity.java
+++ AndroidDemo/src/com/yuejiaoliapidemo/app/DrawableActivity.java
@@ -1,14 +1,33 @@
+
 package com.yuejiaoliapidemo.app;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ClipDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.yuejiaoliapidemo.R;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+public class DrawableActivity extends Activity {
+    private ImageView mImageView;
+    private ImageView mImageView2;
 
-public class DrawableActivity extends AppCompatActivity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_drawable);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawable);
+
+        mImageView = (ImageView) findViewById(R.id.imageView1);
+
+        // BitmapDrawable -> Bitmap
+        BitmapDrawable drawable = (BitmapDrawable) getResources().getDrawable(R.drawable.ic_favor);
+        Bitmap bitmap = drawable.getBitmap();
+        mImageView.setImageDrawable(drawable);
+
+        mImageView2 = (ImageView) findViewById(R.id.imageView2);
+        ClipDrawable clipDrawable = (ClipDrawable) mImageView2.getDrawable();
+        clipDrawable.setLevel(5000);
+    }
 }
